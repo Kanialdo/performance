935ab9ac904495a947597f7950823b7d5cecd58c
Krystian Kaniowski <kanialdo@gmail.com>
2023-03-05 16:22:17 +0100

    Configure hilt (#14)

---
+++ app/src/main/java/pl/krystiankaniowski/performance/PerformanceApplication.kt
@@ -0,0 +1,7 @@
+package pl.krystiankaniowski.performance
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PerformanceApplication : Application()
--- feature-timer/src/main/java/pl/krystiankaniowski/performance/timer/TimerScreen.kt
+++ feature-timer/src/main/java/pl/krystiankaniowski/performance/timer/TimerScreen.kt
@@ -21,10 +21,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import pl.krystiankaniowski.performance.ui.theme.PerformanceTheme
 
 @Composable
-fun TimerScreen(viewModel: TimerViewModel) {
+fun TimerScreen(
+    viewModel: TimerViewModel = viewModel(),
+) {
     TimerScreenContent(
         state = viewModel.state.collectAsState().value,
         onEvent = viewModel::onEvent,
