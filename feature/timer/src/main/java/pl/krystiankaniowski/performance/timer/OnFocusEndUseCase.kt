d60c07af82acffbcabc34ae99c0da2e6a3619ba0
Krystian Kaniowski <kanialdo@gmail.com>
2023-03-20 21:09:19 +0100

    wip

---
+++ feature/timer/src/main/java/pl/krystiankaniowski/performance/timer/OnFocusEndUseCase.kt
@@ -0,0 +1,4 @@
+package pl.krystiankaniowski.performance.timer
+
+class OnFocusEndUseCase {
+}
---
+++ feature/timer/src/main/java/pl/krystiankaniowski/performance/timer/OnFocusStartUseCase.kt
@@ -0,0 +1,6 @@
+package pl.krystiankaniowski.performance.timer
+
+class OnFocusStartUseCase {
+
+    
+}
