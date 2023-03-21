5d74052d9a72b02464f473268e2da2439256c6c9
Krystian Kaniowski <kanialdo@gmail.com>
2023-03-21 22:26:32 +0100

    wip

---
+++ feature/timer/src/main/java/pl/krystiankaniowski/performance/timer/TurnOnDoNotDisturbUseCase.kt
@@ -0,0 +1,23 @@
+package pl.krystiankaniowski.performance.timer
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.ActivityCompat.getSystemService
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class TurnOnDoNotDisturbUseCase @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+
+    operator fun invoke() {
+
+        val notificationManager = requireNotNull(getSystemService(context, NotificationManager::class.java))
+        val isGranted = notificationManager.isNotificationPolicyAccessGranted
+        if (isGranted) {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
+        } else{
+            throw
+        }
+    }
+}
