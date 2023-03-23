639a31de1e6f819509ed31fb56f21c56fc11f620
Krystian Kaniowski <kanialdo@gmail.com>
2023-03-23 17:10:08 +0100

    remove build tpyes from notifications

--- feature/notifications/build.gradle.kts
+++ feature/notifications/build.gradle.kts
@@ -4,18 +4,6 @@ plugins {
 
 android {
     namespace = "pl.krystiankaniowski.performance.notifications"
-
-    buildTypes {
-        debug {
-        }
-        named("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
 }
 
 dependencies {
