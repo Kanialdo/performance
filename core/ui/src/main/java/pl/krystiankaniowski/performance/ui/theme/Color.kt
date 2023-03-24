47b77a264401da1dae6baa87d6de373a7234d625
Krystian Kaniowski <kanialdo@gmail.com>
2023-03-24 21:21:36 +0100

    wip

--- core/ui/src/main/java/pl/krystiankaniowski/performance/ui/theme/Color.kt
+++ core/ui/src/main/java/pl/krystiankaniowski/performance/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package pl.krystiankaniowski.performance.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DeepOrange050 = Color(0xFFFBE9E7)
+val DeepOrange100 = Color(0xFFFFCCBC)
+val DeepOrange200 = Color(0xFFFFAB91)
+val DeepOrange300 = Color(0xFFFF8A65)
+val DeepOrange400 = Color(0xFFFF7043)
+val DeepOrange500 = Color(0xFFFF5722)
+val DeepOrange600 = Color(0xFFF4511E)
+val DeepOrange700 = Color(0xFFE64A19)
+val DeepOrange800 = Color(0xFFD84315)
+val DeepOrange900 = Color(0xFFBF360C)
--- core/ui/src/main/java/pl/krystiankaniowski/performance/ui/theme/Theme.kt
+++ core/ui/src/main/java/pl/krystiankaniowski/performance/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = DeepOrange500,
+    secondary = DeepOrange600,
+    tertiary = DeepOrange700,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = DeepOrange500,
+    secondary = DeepOrange400,
+    tertiary = DeepOrange300,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
