ebcdf1381c3be7ac7ad71c26f9020b64e52b00e2
Krystian Kaniowski <kanialdo@gmail.com>
2023-04-05 23:02:58 +0200

    fix tests

--- feature/settings/src/test/kotlin/pl/krystiankaniowski/performance/settings/SettingsViewModelTest.kt
+++ feature/settings/src/test/kotlin/pl/krystiankaniowski/performance/settings/SettingsViewModelTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import pl.krystiankaniowski.performance.domain.settings.SettingsItem
+import pl.krystiankaniowski.performance.domain.settings.SettingsItems
 import pl.krystiankaniowski.performance.domain.settings.SettingsItemsProvider
 import pl.krystiankaniowski.performance.testing.rule.InstantDispatcherExtension
 
@@ -34,11 +35,13 @@ class SettingsViewModelTest {
     fun `WHEN all providers provided data THEN loaded state is emitted`() = runTest {
         val item1 = SettingsItem.Simple(
             order = 1,
+            category = SettingsItems.Category.ABOUT,
             title = "",
             description = null,
         )
         val item2 = SettingsItem.Simple(
             order = 2,
+            category = SettingsItems.Category.ABOUT,
             title = "",
             description = null,
         )
@@ -55,7 +58,7 @@ class SettingsViewModelTest {
         Assertions.assertEquals(
             viewModel.state.value,
             SettingsViewModel.State.Loaded(
-                items = listOf(item1, item2),
+                items = mapOf(SettingsItems.Category.ABOUT to listOf(item1, item2)),
             ),
         )
     }
