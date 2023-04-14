cb8978642943c6afee3932f2822ba01ef3376ea4
Krystian Kaniowski <kanialdo@gmail.com>
2023-04-14 22:51:14 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
 plugins {
     `kotlin-dsl`
     id("performance.root")
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.detekt)
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.junit5) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.android) apply false
 }
