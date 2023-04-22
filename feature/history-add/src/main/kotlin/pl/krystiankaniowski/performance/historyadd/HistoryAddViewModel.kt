1396c6f19668b7fe848723aacc4505043596d51f
Krystian Kaniowski <kanialdo@gmail.com>
2023-04-23 00:16:19 +0200

    wip

--- feature/history-add/src/main/kotlin/pl/krystiankaniowski/performance/historyadd/HistoryAddViewModel.kt
+++ feature/history-add/src/main/kotlin/pl/krystiankaniowski/performance/historyadd/HistoryAddViewModel.kt
@@ -7,7 +7,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
@@ -66,16 +65,14 @@ class HistoryAddViewModel @Inject constructor(
 
     init {
         if (id != null) {
-            viewModelScope.launch {
+            _state.update {
                 val entry = repository.get(id)
-                _state.update {
-                    State.Loaded(
-                        startDate = entry.startDate.toLocalDateTime(TimeZone.currentSystemDefault()).date,
-                        startTime = entry.startDate.toLocalDateTime(TimeZone.currentSystemDefault()).time,
-                        endDate = entry.endDate.toLocalDateTime(TimeZone.currentSystemDefault()).date,
-                        endTime = entry.endDate.toLocalDateTime(TimeZone.currentSystemDefault()).time,
-                    )
-                }
+                State.Loaded(
+                    startDate = entry.startDate.toLocalDateTime(TimeZone.currentSystemDefault()).date,
+                    startTime = entry.startDate.toLocalDateTime(TimeZone.currentSystemDefault()).time,
+                    endDate = entry.endDate.toLocalDateTime(TimeZone.currentSystemDefault()).date,
+                    endTime = entry.endDate.toLocalDateTime(TimeZone.currentSystemDefault()).time,
+                )
             }
         }
     }
@@ -88,39 +85,29 @@ class HistoryAddViewModel @Inject constructor(
         Event.OnSaveClick -> onSaveButtonClick()
     }
 
-    private fun onStartDateChange(date: LocalDate?) = viewModelScope.launch {
-        _state.updateIf<State.Loaded> {
-            copy(startDate = date)
-        }
+    private fun onStartDateChange(date: LocalDate?) = _state.updateIf<State.Loaded> {
+        copy(startDate = date)
     }
 
-    private fun onStartTimeChange(time: LocalTime?) = viewModelScope.launch {
-        _state.updateIf<State.Loaded> {
-            copy(startTime = time)
-        }
+    private fun onStartTimeChange(time: LocalTime?) = _state.updateIf<State.Loaded> {
+        copy(startTime = time)
     }
 
-    private fun onEndDateChange(date: LocalDate?) = viewModelScope.launch {
-        _state.updateIf<State.Loaded> {
-            copy(endDate = date)
-        }
+    private fun onEndDateChange(date: LocalDate?) = _state.updateIf<State.Loaded> {
+        copy(endDate = date)
     }
 
-    private fun onEndTimeChange(time: LocalTime?) = viewModelScope.launch {
-        _state.updateIf<State.Loaded> {
-            copy(endTime = time)
-        }
+    private fun onEndTimeChange(time: LocalTime?) = _state.updateIf<State.Loaded> {
+        copy(endTime = time)
     }
 
-    private fun onSaveButtonClick() = viewModelScope.launch {
-        _state.runIf<State.Loaded> {
-            check(isSaveButtonEnable)
-            val focus = Focus(
-                startDate = LocalDateTime(checkNotNull(startDate), checkNotNull(startTime)).toInstant(TimeZone.currentSystemDefault()),
-                endDate = LocalDateTime(checkNotNull(endDate), checkNotNull(endTime)).toInstant(TimeZone.currentSystemDefault()),
-            )
-            repository.add(focus)
-            _effects.emit(Effect.CloseScreen)
-        }
+    private fun onSaveButtonClick() = _state.runIf<State.Loaded> {
+        check(isSaveButtonEnable)
+        val focus = Focus(
+            startDate = LocalDateTime(checkNotNull(startDate), checkNotNull(startTime)).toInstant(TimeZone.currentSystemDefault()),
+            endDate = LocalDateTime(checkNotNull(endDate), checkNotNull(endTime)).toInstant(TimeZone.currentSystemDefault()),
+        )
+        repository.add(focus)
+        _effects.emit(Effect.CloseScreen)
     }
 }
