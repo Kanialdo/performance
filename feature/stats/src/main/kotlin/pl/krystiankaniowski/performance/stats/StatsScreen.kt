747375289b4241144e9116696443c060b5962ae8
Krystian Kaniowski <kanialdo@gmail.com>
2023-05-24 18:52:41 +0200

    wip

--- feature/stats/src/main/kotlin/pl/krystiankaniowski/performance/stats/StatsScreen.kt
+++ feature/stats/src/main/kotlin/pl/krystiankaniowski/performance/stats/StatsScreen.kt
@@ -1,21 +1,26 @@
 package pl.krystiankaniowski.performance.stats
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowLeft
 import androidx.compose.material.icons.filled.ArrowRight
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,15 +28,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import pl.krystiankaniowski.performance.model.Seconds
 import pl.krystiankaniowski.performance.ui.components.PerformanceLoadingScreen
-import pl.krystiankaniowski.performance.ui.components.PerformanceUnderDevelopmentScreen
 import pl.krystiankaniowski.performance.ui.theme.PerformanceTheme
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,11 +68,6 @@ fun StatsScreen(
         ) {
             when (val state = viewModel.state.collectAsState().value) {
                 StatsViewModel.State.Loading -> PerformanceLoadingScreen()
-                StatsViewModel.State.UnderDevelopment -> PerformanceUnderDevelopmentScreen()
-                is StatsViewModel.State.Loaded -> StatsScreenContent(
-                    state = state,
-                )
-
                 is StatsViewModel.State.Daily -> StatsScreenContentDaily(
                     state = state,
                 )
@@ -81,55 +85,105 @@ fun StatsScreenContentDaily(state: StatsViewModel.State.Daily) {
             verticalAlignment = Alignment.CenterVertically,
         ) {
             IconButton(
+                enabled = false,
                 onClick = { /*TODO*/ },
                 content = { Icon(imageVector = Icons.Default.ArrowLeft, contentDescription = null) },
             )
             Text(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
                 text = state.date,
                 textAlign = TextAlign.Center,
             )
             IconButton(
+                enabled = false,
                 onClick = { /*TODO*/ },
                 content = { Icon(imageVector = Icons.Default.ArrowRight, contentDescription = null) },
             )
         }
         Divider()
-        Column {
-            Card(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Total: ${state.total}")
-            }
-        }
-    }
-
-    @Suppress("EmptyFunctionBlock", "UnusedPrivateMember")
-    @Composable
-    private fun StatsScreenContent(state: StatsViewModel.State.Loaded) {
-    }
-
-    @Preview
-    @Composable
-    private fun StatsScreenContent_Preview() {
-        PerformanceTheme {
-            StatsScreenContent(
-                state = StatsViewModel.State.Loaded,
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Text("Focus time")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = state.total, style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.primary)
+            Spacer(modifier = Modifier.height(32.dp))
+            Text("Focus time")
+            Spacer(modifier = Modifier.height(16.dp))
+            DailyStat(
+                data = state.chartData,
             )
         }
     }
+}
 
-    @Preview
-    @Composable
-    private fun StatsScreenContentDaily_Preview() {
-        PerformanceTheme {
-            Scaffold {
-                Box(modifier = Modifier.padding(it)) {
-                    StatsScreenContentDaily(
-                        state = StatsViewModel.State.Daily(
-                            date = "2020-02-02",
-                            total = Seconds(0),
-                        ),
+@Suppress("EmptyFunctionBlock", "UnusedPrivateMember")
+@Composable
+private fun DailyStat(modifier: Modifier = Modifier, data: List<StatsViewModel.FocusTime>) {
+    val c = Color.Gray.copy(alpha = 0.5f)
+    Column(modifier = modifier) {
+        BoxWithConstraints(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .border(1.dp, c),
+        ) {
+            val color = MaterialTheme.colorScheme.primary
+            data.forEach {
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                    drawRect(
+                        color = color,
+                        topLeft = Offset(x = this.size.width * it.started, y = 0f),
+                        size = Size(width = this.size.width * it.width, height = this.size.height),
                     )
                 }
             }
         }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = "", fontSize = 14.sp, color = c)
+            Text(text = " 4:00", fontSize = 14.sp, color = c)
+            Text(text = " 8:00", fontSize = 14.sp, color = c)
+            Text(text = "12:00", fontSize = 14.sp, color = c)
+            Text(text = "16:00", fontSize = 14.sp, color = c)
+            Text(text = "20:00", fontSize = 14.sp, color = c)
+            Text(text = "", fontSize = 14.sp, color = c)
+        }
     }
+}
+
+@Preview
+@Composable
+private fun StatsScreenContentDaily_Preview() {
+    PerformanceTheme {
+        Scaffold {
+            Box(modifier = Modifier.padding(it)) {
+                StatsScreenContentDaily(
+                    state = StatsViewModel.State.Daily(
+                        date = "2020-02-02",
+                        total = "1h 30 min",
+                        chartData = listOf(
+                            StatsViewModel.FocusTime(10.hours.inWholeMilliseconds, 10.hours.inWholeMilliseconds + 25.minutes.inWholeMilliseconds),
+                            StatsViewModel.FocusTime(11.hours.inWholeMilliseconds, 11.hours.inWholeMilliseconds + 25.minutes.inWholeMilliseconds),
+                        ),
+                    ),
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DailyStat_Preview() {
+    PerformanceTheme {
+        DailyStat(
+            data = listOf(
+                StatsViewModel.FocusTime(10.hours.inWholeMilliseconds, 10.hours.inWholeMilliseconds + 25.minutes.inWholeMilliseconds),
+                StatsViewModel.FocusTime(11.hours.inWholeMilliseconds, 11.hours.inWholeMilliseconds + 25.minutes.inWholeMilliseconds),
+            ),
+        )
+    }
+}
--- feature/stats/src/main/kotlin/pl/krystiankaniowski/performance/stats/StatsViewModel.kt
+++ feature/stats/src/main/kotlin/pl/krystiankaniowski/performance/stats/StatsViewModel.kt
@@ -10,22 +10,38 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
-import pl.krystiankaniowski.performance.model.Seconds
+import pl.krystiankaniowski.performance.domain.localization.time.DurationFormatter
+import pl.krystiankaniowski.performance.domain.stats.FocusRepository
+import pl.krystiankaniowski.performance.model.toSeconds
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.days
 
 @HiltViewModel
-class StatsViewModel @Inject constructor() : ViewModel() {
+class StatsViewModel @Inject constructor(
+    private val repository: FocusRepository,
+    private val durationFormatter: DurationFormatter,
+) : ViewModel() {
 
     sealed interface State {
         object Loading : State
-        object UnderDevelopment : State
-        object Loaded : State
         data class Daily(
             val date: String,
-            val total: Seconds,
+            val total: String,
+            val chartData: List<FocusTime>,
         ) : State
+    }
+
+    data class FocusTime(
+        val milisStart: Long,
+        val milisEnd: Long,
+    ) {
+        val started: Float = (milisStart / 1.days.inWholeMilliseconds.toFloat())
+        val width: Float = ((milisEnd - milisStart) / 1.days.inWholeMilliseconds.toFloat())
     }
 
     sealed interface Effect
@@ -38,11 +54,24 @@ class StatsViewModel @Inject constructor() : ViewModel() {
 
     init {
         viewModelScope.launch {
-            _state.update {
-                State.Daily(
-                    date = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date.toString(),
-                    total = Seconds(0),
-                )
+            val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+            repository.getAll().collect {
+                val events = it.filter {
+                    it.startDate >= now.date.atStartOfDayIn(TimeZone.currentSystemDefault()) && it.endDate < now.date.plus(DateTimeUnit.DayBased(1))
+                        .atStartOfDayIn(TimeZone.currentSystemDefault())
+                }
+                _state.update {
+                    State.Daily(
+                        date = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date.toString(),
+                        total = durationFormatter.format(events.sumOf { (it.endDate - it.startDate).inWholeSeconds }.toSeconds()),
+                        chartData = events.map {
+                            FocusTime(
+                                it.startDate.toLocalDateTime(TimeZone.currentSystemDefault()).time.toMillisecondOfDay().toLong(),
+                                it.endDate.toLocalDateTime(TimeZone.currentSystemDefault()).time.toMillisecondOfDay().toLong(),
+                            )
+                        },
+                    )
+                }
             }
         }
     }
