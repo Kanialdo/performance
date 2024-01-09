12eba368d46e5125875b5a9e6954641a78e17fa1
Krystian Kaniowski <kaniowski@smartpatient.eu>
2024-01-09 09:31:03 +0100

    Bump target sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "pl.krystiankaniowski.performance"
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
